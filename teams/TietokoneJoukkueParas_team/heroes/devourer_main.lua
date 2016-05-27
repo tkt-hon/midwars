622e603e087e5fe7c0b5527b249bbdda950ec8f2
Sami Saada <saada@cs.helsinki.fi>
2016-05-27 13:00:41 +0300

    tjp devo loads generics

--- teams/TietokoneJoukkueParas_team/heroes/devourer_main.lua
+++ teams/TietokoneJoukkueParas_team/heroes/devourer_main.lua
@@ -35,7 +35,7 @@ runfile "bots/metadata.lua"
 runfile "bots/behaviorLib.lua"
 runfile "bots/teams/TietokoneJoukkueParas_team/generics.lua"
 
-local core, eventsLib, behaviorLib, metadata, skills = object.core, object.eventsLib, object.behaviorLib, object.metadata, object.skills
+local core, eventsLib, behaviorLib, metadata, skills, generics = object.core, object.eventsLib, object.behaviorLib, object.metadata, object.skills, object.generics
 
 local print, ipairs, pairs, string, table, next, type, tinsert, tremove, tsort, format, tostring, tonumber, strfind, strsub
   = _G.print, _G.ipairs, _G.pairs, _G.string, _G.table, _G.next, _G.type, _G.table.insert, _G.table.remove, _G.table.sort, _G.string.format, _G.tostring, _G.tonumber, _G.string.find, _G.string.sub
